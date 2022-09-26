bb7eccbbcf7de6157c72f3357125b9e1abe39381
Bezemoon <aybolkabiev@gmail.com>
2022-09-26 16:56:53 +0600

    Changed Camera Movement

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,16 +1,64 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using Boxes;
+using Interfaces;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
+[RequireComponent(typeof(Camera))]
+public class CameraMovement : MonoBehaviour, IMovable
 {
-    [SerializeField]  private Camera _camera;
-    [SerializeField] private GameManager _manager;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Transform _transform;
+    [SerializeField] private BoxSpawner _boxSpawner;
 
-    private float elapsedTime;
-    private void Update()
+    private readonly float _borderHeight = 0.5f;
+
+    public event Action<Color> BackgroundColorChanged;
+
+    private void Start()
     {
-        if (_camera.WorldToViewportPoint(new Vector3(0, _manager.CurrentBlock.transform.position.y, 0)).y >= 0.5f)
-            transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime, transform.position.z);
+        BackgroundColorChanged?.Invoke(_boxSpawner.CurrentBox.Material.color);
+    }
+    
+    private void OnEnable()
+    {
+        _boxSpawner.CameraMoved += OnCameraMove;
+    }
+
+    private void OnDisable()
+    {
+        _boxSpawner.CameraMoved -= OnCameraMove;
+    }
+
+    private void OnCameraMove(Box box)
+    {
+        if(GetViewportPoint(box.Transform.position) >= _borderHeight)
+        {
+            StartCoroutine(CameraMoving(box));
+            BackgroundColorChanged?.Invoke(box.Material.color);
+        }
+    }
+
+    IEnumerator CameraMoving(Box box)
+    {
+        var viewportPoint = GetViewportPoint(box.Transform.position);
+        while (viewportPoint >= _borderHeight)
+        {
+            Move();
+            viewportPoint = GetViewportPoint(box.Transform.position);
+            yield return null;
+        }
+    }
+
+    private float GetViewportPoint(Vector3 boxPosition)
+    {
+        return _camera.WorldToViewportPoint(new Vector3(0, boxPosition.y, 0)).y;
+    }
+
+    public void Move()
+    {
+        Vector3 position = transform.position;
+        position.y += Time.deltaTime;
+        _transform.position = position;
     }
 }
